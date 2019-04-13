5d7aaaf419298486b0195f5ffae0c4215bfdd968
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-04-13 13:18:00 -0700

    first mvp

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 
 // App setup
 const app = express();
-
 const server = app.listen(5000, () => {
     console.log('Listening at port 5000');
 });
@@ -15,11 +14,12 @@ const server = app.listen(5000, () => {
 app.use(express.static('public'));
 
 // Body-parser middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// parse application/json
-app.use(bodyParser.json())
+// initialise PUG
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // socket setup
 let io = server_socket(server);
@@ -27,13 +27,18 @@ let io = server_socket(server);
 io.on('connection', (socket) => {
     console.log('made socket connection', socket.id);
 
-    // socket.on('addDonation', data => {
-    //     // save to mongodb 
-    //     AddDonations(data);
-    // });
+    socket.on('addDonation', data => {
+        setTimeout(() => {
+            io.sockets.emit('newDonation', data);
+        }, 10);
+    });
 });
 
 // Express routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index');
+});
+
+app.get('/donate', (req, res) => {
+    res.render('donate');
 });
--- public/main.js
+++ public/main.js
@@ -1,1 +1,37 @@
-console.log("main.js")
+const socketFun = (() => {
+    // make socket connection
+    let socket = io.connect('http://localhost:5000');
+    return {
+        getSocket: () => { return socket }
+    }
+})();
+
+socketFun.getSocket().on('newDonation', newDonation => {
+    console.log(newDonation);
+    let totalAmount = document.getElementById('totalAmount');
+    if (totalAmount)
+        totalAmount.innerHTML = convertStringToNumber(totalAmount.innerHTML) + convertStringToNumber(newDonation.amount);
+});
+
+let convertStringToNumber = (text) => {
+    if (Number(text) == NaN)
+        return 0;
+    else
+        return Number(text);
+}
+
+window.onload = () => {
+    let submit = document.getElementById('submit');
+
+    if (submit) {
+        // donate submit
+        submit.addEventListener('click', () => {
+            let amount = convertStringToNumber(document.getElementById('amount').value);
+            let donation = {
+                amount: amount < 0 ? 0 : amount
+            }
+            socketFun.getSocket().emit('addDonation', donation);
+        });
+    }
+
+};
